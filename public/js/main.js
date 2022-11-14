b36d90a6f3eb67dc05158a76f376499cf8047ef7
Ribbal Hussain <ahmedhussain32000@gmail.com>
2022-11-14 11:23:03 -0600

    Implemented sliding mini cart and changed background color. Sliding cart now works on all pages, and added proceed to checkout button. Sti ll required to populate mini cart with user items.

--- public/js/main.js
+++ public/js/main.js
@@ -14,4 +14,13 @@ function init() {
 
 init();
 
+//Adding a shopping cart
 
+const cart = document.querySelector('.fa-cart-shopping')
+const menu = document.querySelector('.mini-cart')
+const closeCart = document.querySelector('#mini-cart-close')
+
+cart.addEventListener('click', () => {
+  cart.classList.toggle('is-active') 
+  menu.classList.toggle('is-active') 
+})
