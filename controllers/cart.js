c70c29c58476bd2e0189bef0cbe8c22e904d9221
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-01-02 11:42:44 -0600

    Fixed currentProduct tracing functionality, data is now being logged on form submission, however data is too packed and needs to be filtered down to only provided fields such as name and size

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,6 +3,7 @@ const checkout = require("../models/Product")
 const mongoose = require("mongoose");
 const User = require("../models/User");
 const { ObjectId } = require("mongodb");
+const Product = require("../models/Product");
 
 module.exports = {
     getCart: (req, res) => {
@@ -17,12 +18,16 @@ module.exports = {
         const ownsProdcut = req.user.shoppingCart.some(product => {
             product.ObjectId == ''
         })
+
+        const currentProduct = Product.find(
+            {name : `${req.body.name}`}, {size : `${req.body.size}`}).lean()
+            console.log(currentProduct)
         req.user.shoppingCart.push({
             id: new ObjectId('63ad232679ffa233cec0298d'),
             quantity: 1,
           });
           await req.user.save()
-        console.log(req.body)
+        // console.log(req.body)
         res.redirect(req.get('referer'));
     },
 
