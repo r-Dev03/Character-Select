26efaac7ad4949c6a130fc46e4d803e9e49fac89
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-01-21 22:53:46 -0600

    General testing

--- controllers/tops.js
+++ controllers/tops.js
@@ -1,12 +1,14 @@
 const { get } = require('mongoose');
 const { getCart } = require('./cart')
 const User = require("../models/User");
+const Product = require('../models/Product');
 
 module.exports = {
     getTops: async (req, res) => {
       try{
+        const tops = await Product.find({category: "Tops"})
         const cart = await getCart(req.user._id)
-        res.render("tops.ejs", {user: req.user, userCart: cart.items});
+        res.render("tops.ejs", {user: req.user, userCart: cart.items, tops: tops});
       }    
       catch(err){
         console.log(err)
