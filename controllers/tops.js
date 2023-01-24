725afc2a2512183c036b24a6df3e1bcc3fcccbdb
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-01-23 22:49:15 -0600

    Fixed mini cart display issues on tops and bottoms page. Fixed tops page not displaying for logged out users. Passed in guest information for logged out users.

--- controllers/tops.js
+++ controllers/tops.js
@@ -4,14 +4,18 @@ const User = require("../models/User");
 const Product = require('../models/Product');
 
 module.exports = {
-    getTops: async (req, res) => {
-      try{
-        const tops = await Product.find({category: "Tops", size: "small"})
+  getTops: async (req, res) => {
+    try{
+      const tops = await Product.find({category: "Tops", size: "small"})
+      if(req.user){
         const cart = await getCart(req.user._id)
-        res.render("tops.ejs", {user: req.user, userCart: cart.items, tops: tops});
-      }    
-      catch(err){
-        console.log(err)
+        res.render('tops.ejs', {user: req.user, userCart: cart.items, tops: tops});
+      }else{
+        res.render('tops.ejs', {user: {name: 'Anonymous'}, userCart:[{name: "No items in cart..."}], tops: tops});
       }
-  },
+    }    
+    catch(err){
+      console.log(err)
+    }
+},
 };
