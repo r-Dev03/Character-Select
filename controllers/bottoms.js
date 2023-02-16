79f402a9fc4db5893ee4046254a50e97f6cf4b9c
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-02-15 20:43:15 -0600

    Implemented bottoms filtering and rendering through bottoms controller. Adding bottoms products to cart now works, on products page as well. Fixed products page navbar links not working. changed href links from relative to absolute.

--- controllers/bottoms.js
+++ controllers/bottoms.js
@@ -1,16 +1,17 @@
 const { get } = require('mongoose');
 const { getCart } = require('./cart')
 const User = require("../models/User");
+const Product = require('../models/Product');
 
 module.exports = {
     getBottoms: async (req, res) => {
       try{
-        // const tops = await Product.find({category: "Bottoms", size: "small"})
+        const bottoms = await Product.find({category: "Bottoms", size: "small"})
         if(req.user){
           const cart = await getCart(req.user._id)
-          res.render('bottoms.ejs', {user: req.user, userCart: cart.items});
+          res.render('bottoms.ejs', {user: req.user, userCart: cart.items, bottoms: bottoms, req: req});
         }else{
-          res.render('bottoms.ejs', {user: {name: 'Anonymous'}, userCart:[]});
+          res.render('bottoms.ejs', {user: {name: 'Anonymous'}, userCart:[], bottoms: bottoms, req: req});
         }
       }    
       catch(err){
