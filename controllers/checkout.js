e289861f97655aaa3a273a34c4bdb729ba951b20
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-02-16 17:11:52 -0600

    Fixed checkout button styling issue on other pages. Added uesr cart to checkout controller and checkout page now renders properly. Added jS file script on checkout page so usercart can be functional.

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,8 +1,23 @@
-const cloudinary = require("../middleware/cloudinary");
-const checkout = require("../models/Product")
+const { get } = require('mongoose');
+const { getCart } = require('./cart')
+const User = require("../models/User");
 
-module.exports = {
-    getCheckout: (req, res) => {
-      res.render("checkout.ejs");
-    }
-    }
+
+// module.exports = {
+//     getCheckout: (req, res) => {
+//       res.render("checkout.ejs");
+//     }
+//     }
+
+    module.exports = {
+      getCheckout: async (req, res) => {
+          try{
+            const cart = await getCart(req.user._id)
+            res.render("checkout.ejs", {user: req.user, userCart: cart.items});
+          }    
+          catch(err){
+            console.log(err)
+          }
+      },
+    };
+    
