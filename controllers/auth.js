d4213eea5c1e03fa4e40c6ec6f8fe6656ab553af
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-03-06 19:29:58 -0600

    Added new Continue As Guest button on signup page that routes to login/?Guest. Passed in new arguments on auth controller for getLogin method to allow for query rendering on login page. Currently work in progress.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 const validator = require("validator");
 const User = require("../models/User");
-//Test
 
 exports.getLogin = (req, res) => {
   if (req.user) {
@@ -9,6 +8,9 @@ exports.getLogin = (req, res) => {
   }
   res.render("login", {
     title: "Login",
+    req: req,
+    guest: "Guest",
+    guestPW: "GuestUser",
   });
 };
 
@@ -45,19 +47,8 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-// exports.logout = (req, res) => {
-//   req.logout(() => {
-//     console.log('User has logged out.')
-//   })
-//   req.session.destroy((err) => {
-//     if (err)
-//       console.log("Error : Failed to destroy the session during logout.", err);
-//     req.user = null;
-//     res.redirect("/");
-//   });
-// };
 
-//new logout method
+//logout method
 exports.logout = (req, res, next) => {
   req.logout((err) => {
     if (err) {
